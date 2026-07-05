940d845457ba51e1a00a7d247c1fc287c5166ac0
odinwalker <266474446+odinwalker@users.noreply.github.com>
2026-07-05 22:40:49 +0800

    难点在处理多个参数并排除掉

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+/*const removeFromArray = function(arry, num) {
+    const arry = [];
+    for (let i = 0; i < arry.length; i++){
+        let index = arry.indexOf(num);
+        arry.splice(index, 1);
+    }
+
+    return arry;
+
 };
+*/
+
+const removeFromArray = function(arry, ...args){
+    return arry.filter(item => !args.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
