d06397f37a989050257e6ec7c967856a53f05f9e
odinwalker <266474446+odinwalker@users.noreply.github.com>
2026-07-05 20:22:06 +0800

    字符串是不可变的，只有split 一个新的数组

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    if (str === ""){
+        return str = ""
+    }
+    else {
+        newStr = str.split("");
+        revStr = newStr.reverse();
+        joinStr = revStr.join("");
+        return joinStr;
+    }
+    // if...else有点多余了
+    /*直接这样
+    return str.split("").reverse().join("");
+    */
 };
+
+console.log(reverseString("life is a gift"));
 
 // Do not edit below this line
 module.exports = reverseString;
